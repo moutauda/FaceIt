541f45c801981d7804f2d7e8cda9ad276c3f7071
Alexandre Moutaud <alexandre.moutaud@cgi.com>
2017-11-23 16:57:57 +0100

    First commit

---
+++ Faceit/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Faceit
+//
+//  Created by Developer on 23/10/2017.
+//  Copyright © 2017 CGI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var faceView: FaceView!{
+        didSet{
+            updateUI()
+        }
+    }
+
+    var expression = FacialExpression(eyes: .close, mouth: .grin){
+        didSet {
+            updateUI()
+        }
+    }
+    c
+    private func updateUI()
+    {
+        switch expression.eyes {
+        case .open:
+            faceView?.eyesOpen = true
+        case .close:
+            faceView?.eyesOpen = false
+        case .squinting:
+            faceView?.eyesOpen = false
+        }
+        
+        faceView?.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+        
+        
+    }
+  
+    
+    private let mouthCurvatures = [FacialExpression.Mouth.grin: 0.5,
+                                   .frown:      -1.0,
+                                   .smile:      1.0,
+                                   .neutral:    0.0,
+                                   .smirk:      -0.5]
+    
+    
+}
+
